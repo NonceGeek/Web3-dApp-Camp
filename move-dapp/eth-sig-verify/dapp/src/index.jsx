6468916f4e117c02d471b317e57340f441118ad4
leeduckgo <albertschr@163.com>
2022-08-04 12:16:33 +0800

    update eth-sig-verifier

--- move-dapp/eth-sig-verify/dapp/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import { App } from "./app";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./style.css";
-
-createRoot(document.getElementById("root")).render(<App />);
