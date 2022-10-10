5f2bb110718a49a7a1cda44cc49025df871b142b
leeduckgo <albertschr@163.com>
2022-10-10 12:09:04 +0800

    update my-lib

--- move-dapp/addr-aggregator/dapp/src/txs/config.js
+++
@@ -1,4 +0,0 @@
-export const COUNTER_ADDRESS = "0x1168e88ffc5cec53b398b42d61885bbb"
-export const COUNTER_RESOURCE_ID = "MyCounter::Counter"
-export const INCR_COUNTER_FUNC_NAMW = "MyCounter::incr_counter"
-export const INCR_COUNTERBY_FUNC_NAME = "MyCounter::incr_counter_by"
